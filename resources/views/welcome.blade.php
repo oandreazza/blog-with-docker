c5a10da53d323f350e7c3b081e544fcd255da738
Mauricio Sganderla <mauriciosganderla@CanoasTEC.local>
2016-06-27 17:12:57 -0300

    some changes for docker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Hello World from Brazil with Docker My Friend from Canoastec! YEAAAAAAH</div>
             </div>
         </div>
     </body>
